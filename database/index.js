56f32cd0f49f16cf2579e6360e9b5bbc1c2d2ae2
Chad Cramer <chcr6493@colorado.edu>
2019-06-10 11:46:22 -0500

    Postgress installed, seeded, and am currently converting it to be my primary database. Adding connection pool and CRUD paths

--- database/index.js
+++ database/index.js
@@ -1,43 +1,63 @@
-const mongoose = require("mongoose");
+const { Pool, Client } = require('pg');
+const postgressInfo = require('./postgresInfo');
 
-mongoose.connect('mongodb://localhost/sdc', {useNewUrlParser: true}, (err) => {
+const pool = new Pool({
+  user: postgressInfo.user,
+  host: postgressInfo.host,
+  database: postgressInfo.database,
+  password: postgressInfo.password,
+  port: postgressInfo.port,
+})
+
+pool.connect((err, success) => {
   if(err){
-      console.log('error connecting to mongodb: ', err);
+    console.log('error connecting to postgress db:', err);
   } else {
-      console.log("successful connection to mongodb");
+    console.log('success connecting to postgres db');
   }
-});
+})
 
-let reviewSchema = mongoose.Schema({
-  sku: Number,
-  user: String,
-  date: String,
-  title: String,
-  description: String
-});
 
-let customerReview = mongoose.model("customerReview", reviewSchema, 'sole');
+// const mongoose = require("mongoose");
 
-let save = (file, callBack) => {
-  customerReview.insertMany(file, (err, res) => {
-    if (err) {
-      console.log(err, null);
-    } else {
-      console.log(err);
-    }
-  });
-};
+// mongoose.connect('mongodb://localhost/sdc', {useNewUrlParser: true}, (err) => {
+//   if(err){
+//       console.log('error connecting to mongodb: ', err);
+//   } else {
+//       console.log("successful connection to mongodb");
+//   }
+// });
 
-let findAll = (obj, callBack) => {
-  customerReview.find(obj, function(err, docs) {
-    if (err) {
-      console.log("I AM GETTING AN ERROR");
-    } else {
-      console.log("sucess retrieving database stuff");
-      callBack(null, docs);
-    }
-  });
-};
+// let reviewSchema = mongoose.Schema({
+//   sku: Number,
+//   user: String,
+//   date: String,
+//   title: String,
+//   description: String
+// });
+
+// let customerReview = mongoose.model("customerReview", reviewSchema, 'sole');
+
+// let save = (file, callBack) => {
+//   customerReview.insertMany(file, (err, res) => {
+//     if (err) {
+//       console.log(err, null);
+//     } else {
+//       console.log(err);
+//     }
+//   });
+// };
+
+// let findAll = (obj, callBack) => {
+//   customerReview.find(obj, function(err, docs) {
+//     if (err) {
+//       console.log("I AM GETTING AN ERROR");
+//     } else {
+//       console.log("sucess retrieving database stuff");
+//       callBack(null, docs);
+//     }
+//   });
+// };
 
 // customerReview.create(reviews, (err)=>{
 //   if (err){
@@ -51,4 +71,5 @@ let findAll = (obj, callBack) => {
 
 // module.exports.findAll = findAll;
 // module.exports.save = save;
-module.exports = customerReview;
+// module.exports = customerReview;
+module.exports = pool;
